2072f5ec9c6edd1d82baf3a4d1e812fa254b7787
daniel Bran <recuerdosdiarios4@gmail.com>
2025-03-21 11:03:13 -0600

    Agregar archivos de proyecto.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace gestor_de_tareas_personales_practica.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
